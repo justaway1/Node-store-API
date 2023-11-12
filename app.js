0904ae74005bb18b5dd9260ebb246eb95a20975d
Stanislav Angelov <stannyweb@gmail.com>
2023-11-12 14:45:55 +0200

    Setting middlewares

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Second commit')
+//Imports
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+
+require('dotenv').config()
+const errorHandler = require('./middlewares/error-handler')
+const notFoundMiddleware = require('./middlewares/not-found')
+
+//middlewares
+app.use(express.json())
+
+//routes
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Store API</h1><a href='/api/v1/products'>Products list</a>`)
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    //connect DB
+    app.listen(port, console.log(`Server is listening on ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
