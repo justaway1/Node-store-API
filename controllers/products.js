41fbeb67b729560e89b08c334431a7cb6ac0e8e1
Stanislav Angelov <stannyweb@gmail.com>
2023-11-15 19:22:23 +0200

    Setting Query filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,12 @@
+const Product = require('../modules/product')
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: `All products for testing` })
+  const products = await Product.find({ company: 'marcos' })
+  res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: `All product routes` })
+  const product = await Product.find(req.query)
+  res.status(200).json({ product, nbHits: product.length })
 }
 
 module.exports = { getAllProducts, getAllProductsStatic }
--- populate.js
+++ populate.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const connectDB = require('./db/connect')
 
-const Product = require('./modules/Product')
+const Product = require('./modules/product')
 const jsonProduct = require('./products.json')
 
 const start = async () => {
@@ -11,8 +11,10 @@ const start = async () => {
     await connectDB(process.env.MONGO_URI)
     await Product.deleteMany()
     await Product.create(jsonProduct)
+    process.exit(0)
   } catch (error) {
     console.log(error)
+    process.exit(1)
   }
 }
 
