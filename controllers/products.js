13899a863832c9e344e5fdc29846ed96493be1e8
Stanislav Angelov <stannyweb@gmail.com>
2023-11-18 20:33:03 +0200

    queries added

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,23 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-  const product = await Product.find(req.query)
+  const { features, company, name } = req.query
+  const queryObject = {}
+
+  if (features) {
+    queryObject.features = features === 'true' ? true : false
+  }
+
+  if (company) {
+    queryObject.company = company
+  }
+
+  if (name) {
+    queryObject.name = name
+  }
+
+  console.log(queryObject)
+  const product = await Product.find(queryObject)
   res.status(200).json({ product, nbHits: product.length })
 }
 
