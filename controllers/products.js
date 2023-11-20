7e95d08a40cf942eb85e69e5680a00394c70faa0
Stanislav Angelov <stannyweb@gmail.com>
2023-11-20 11:14:49 +0200

    Added Numeric Filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,14 @@
 const Product = require('../modules/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name').select('name company')
+  //${gt - greater than lt - less than}
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('name')
+    .select('rating price')
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-  const { features, company, name, sort, fields } = req.query
+  const { features, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
 
   if (features) {
@@ -20,11 +23,33 @@ const getAllProducts = async (req, res) => {
     //Регулярен израз, с който търсим по зададения query parametar
     queryObject.name = { $regex: name, $options: 'i' }
   }
+  //Numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '<': '$lt',
+      '>=': '$gte',
+      '<=': '$lte',
+      '=': '$eq'
+    }
+    const regEx = /\b(<|>|>=|<=|=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      match => `-${operatorMap[match]}-`
+    )
 
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
+  console.log(queryObject)
   let result = Product.find(queryObject)
   // Sort
   if (sort) {
-    console.log(sort)
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
